c0c4c031ee2c9270cf11aff7193cd5405e0a2f67
Thillak-kumar <69104052+Thillak-kumar@users.noreply.github.com>
2022-11-26 19:30:55 +0530

    Add files via upload

---
+++ website_scrapping.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time,re
+url="https://ful.io/"
+driver=webdriver.Chrome(executable_path=r"C:\Users\mlthi\OneDrive\Desktop\project\10th class results\driver\chromedriver.exe")
+driver.maximize_window()
+driver.get(url)
+time.sleep(15)
+social_links=[]
+c=""
+email=0
+elems = driver.find_elements(By.XPATH,"//a[@href]")
+for elem in elems:
+    s=elem.get_attribute("href")
+    obj1 = re.findall('([\w\-\.]+)',s)
+    if(obj1[1]=="www.linkedin.com") or (obj1[1]=="www.facebook.com"):
+        social_links.append(s)
+    if obj1[1]=="support":
+        email=str(obj1[1]+obj1[2])
+    if obj1[0]=="tel":
+       c="+"+" ".join(obj1[1:])
+driver.close()
+print("Social links -")
+print(social_links)
+print("Email/s")
+print(email)
+print("Contact:")
+print(c)
+
+
